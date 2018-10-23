891eb9a693cff2731b46fa2a8e2392d328d74dfe
USER-20161225ZW\Administrator <ffxbao@gmail.com>
2018-10-23 17:03:39 +0800

    no message

--- seckill/src/main/sql/schema.sql
+++ seckill/src/main/sql/schema.sql
@@ -6,12 +6,12 @@ CREATE DATABASE seckill;
 use seckill;
 --创建秒杀库存表
 CREATE TABLE seckill(
-'seckill_id' bigint  NOT NULL AUTO_INCREMENT COMMENT '商品库存id',
-'name' varchar(120) NOT NULL COMMENT '商品名称',
-'number' int NOT NULL COMMENT '库存数量',
-'start_time' timestamp NOT NULL COMMENT '秒杀开启时间',
-'end_time' timestamp NOT NULL COMMENT '秒杀结束时间',
-'create_time' timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+seckill_id bigint  NOT NULL AUTO_INCREMENT COMMENT '商品库存id',
+name varchar(120) NOT NULL COMMENT '商品名称',
+number int NOT NULL COMMENT '库存数量',
+start_time timestamp NOT NULL COMMENT '秒杀开启时间',
+end_time timestamp NOT NULL COMMENT '秒杀结束时间',
+create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
 PRIMARY KEY (seckill_id),
 key idx_start_time(start_time),
 key idx_end_time(end_time),
@@ -31,16 +31,16 @@ values
 --秒杀成功明细表
 --用户登录认证相关信息
 CREATE TABLE success_killed(
-'seckill_id' bigint NOT NULL COMMENT '秒杀商品id',
-'user_phone' bigint NOT NULL COMMENT '用户手机号',
-'state' tinyint NOT NULL DEFAULT -1 COMMENT '状态标识：-1无效 0成功 1已付款 2已发货',
-'create_time' timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-PRIMARY KEY (seckill_id,user_phone),--联合主键
+seckill_id bigint NOT NULL COMMENT '秒杀商品id',
+user_phone bigint NOT NULL COMMENT '用户手机号',
+state tinyint NOT NULL DEFAULT -1 COMMENT '状态标识：-1无效 0成功 1已付款 2已发货',
+create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+PRIMARY KEY (seckill_id,user_phone),/*联合主键*/
 key idx_create_time(create_time)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='秒杀成功明细表'
 
 --连接数据库控制台
-mysql -uroot -p
+--mysql -uroot -p
 
 --为什么手写DDL？
 --记录每次上线的DDL修改
